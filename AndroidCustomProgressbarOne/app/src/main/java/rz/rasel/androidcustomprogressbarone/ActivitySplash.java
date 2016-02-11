5cd1e3a63191de498b5062ec0af13e08809002ba
Rz Rasel <emailrzrasel@gmail.com>
2016-02-11 10:26:05 +0600

    Add Project:- Android Custom Progress Bar One

---
+++ AndroidCustomProgressbarOne/app/src/main/java/rz/rasel/androidcustomprogressbarone/ActivitySplash.java
@@ -0,0 +1,21 @@
+package rz.rasel.androidcustomprogressbarone;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ActivitySplash extends AppCompatActivity {
+    //|------------------------------------|
+    //|~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        //|------------------------------------|
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        //|------------------------------------|
+        startActivity(new Intent(getApplicationContext(), ActivityProgressBar.class));
+        finish();
+        //|------------------------------------|
+    }
+    //|~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|
+}
